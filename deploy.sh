af043e6099764e37be2894681c63ba57c7af94e3
Vitaly <onebeattrue@gmail.com>
2025-01-23 02:43:58 +0300

    Фикс: сохранение бд при перезапуске

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,14 @@ git pull origin master
 # Удаление старого контейнера, если он существует
 docker rm -f my-telegram-bot || true
 
+# Создание volume для базы данных, если его еще нет
+docker volume create telegram-bot-db || true
+
 # Построение нового образа из Dockerfile
 docker build -t telegram-bot .
 
-# Запуск нового контейнера с примонтированной БД
-docker run -d --name my-telegram-bot --restart unless-stopped telegram-bot
-
+# Запуск нового контейнера с примонтированным volume для БД
+docker run -d --name my-telegram-bot \
+  --restart unless-stopped \
+  -v telegram-bot-db:/app \
+  telegram-bot
